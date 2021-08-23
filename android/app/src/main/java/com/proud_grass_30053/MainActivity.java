e0676d27de020c790133fb3b6271d7c9a839a015
Crowdbotics <team@crowdbotics.com>
2021-08-23 18:58:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/proud_grass_30053/MainActivity.java
@@ -0,0 +1,15 @@
+package com.proud_grass_30053;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "proud_grass_30053";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_grass_30053Identifier",
+  url: "https://proud_grass_30053Identifier.botics.co",
+  api: "https://proud_grass_30053Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
